93fbe846eb97210edfac105bffe2cb732a221e1e
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-02-26 11:30:49 -0300

    ci: mongodb docker image and migration

---
+++ init-mongo.js
@@ -0,0 +1,6 @@
+db.createCollection('TotalCombinationGenerationAccordingNumberOfEntries');
+
+db.TotalCombinationGenerationAccordingNumberOfEntries.insertMany([
+  { numberOfEntries: 1, count: 0 },
+  { numberOfEntries: 2, count: 0 }
+]);
