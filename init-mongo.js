26ba98285ab3790207946092ef00b85f1936d53d
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-02-29 09:26:18 -0300

    feat: new collection in mongo initializing and populate with first limited combinations

--- init-mongo.js
+++ init-mongo.js
@@ -1,6 +1,69 @@
-db.createCollection('TotalCombinationGenerationAccordingNumberOfEntries');
+db.createCollection('TotalCombinationGenerationAccordingNumberOfInputs');
 
-db.TotalCombinationGenerationAccordingNumberOfEntries.insertMany([
-  { NumberOfEntries: 1, Count: 0 },
-  { NumberOfEntries: 2, Count: 0 }
+db.TotalCombinationGenerationAccordingNumberOfInputs.insertMany([
+  { NumberOfInputs: 1, Count: 0 },
+  { NumberOfInputs: 2, Count: 0 }
+]);
+
+db.createCollection('Combinations');
+
+db.Combinations.insertMany([
+  {
+    NumberOfInputs: 2,
+    NumberOfSprints: 1,
+    NumberOfCombinationsPerSprint: 1,
+    Sprints: [
+      {
+        Combinations: [
+          {
+            PairOne: 0,
+            PairTwo: 1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    NumberOfInputs: 3,
+    NumberOfSprints: 3,
+    NumberOfCombinationsPerSprint: 2,
+    Sprints: [
+      {
+        Combinations: [
+          {
+            PairOne: 0,
+            PairTwo: 1
+          },
+          {
+            PairOne: 2,
+            PairTwo: 2
+          }
+        ]
+      },
+      {
+        Combinations: [
+          {
+            PairOne: 0,
+            PairTwo: 2
+          },
+          {
+            PairOne: 1,
+            PairTwo: 1
+          }
+        ]
+      },
+      {
+        Combinations: [
+          {
+            PairOne: 1,
+            PairTwo: 2
+          },
+          {
+            PairOne: 0,
+            PairTwo: 0
+          }
+        ]
+      }
+    ]
+  }
 ]);
